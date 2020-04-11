1262f76d4165e972504004e8a0f8c57d3e06edb3
Nijat <nmursali2019@ada.edu.az>
2020-04-12 00:20:02 +0400

    done

--- todolistapp/src/app/app.module.ts
+++ todolistapp/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
